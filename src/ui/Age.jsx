0bbb31bcd31e6c811a45886b4b5e08f1a7978454
defy <8841170+defydef@users.noreply.github.com>
2023-11-02 07:32:49 +0700

    Refactor parts of AgeDisplay

---
+++ src/ui/Age.jsx
@@ -0,0 +1,14 @@
+function Age({ label }) {
+  return (
+    <div className="flex items-start gap-[8px] relative self-stretch w-full flex-[0_0_auto]">
+      <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-purple text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap text-[var(--purple)]">
+        - -
+      </div>
+      <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-black text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
+        {label}
+      </div>
+    </div>
+  );
+}
+
+export default Age;
--- src/ui/AgeDisplay.jsx
+++ src/ui/AgeDisplay.jsx
@@ -1,30 +1,11 @@
+import Age from "./Age";
+
 function AgeDisplay() {
   return (
     <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
-      <div className="flex items-start gap-[8px] relative self-stretch w-full flex-[0_0_auto]">
-        <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-purple text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
-          - -
-        </div>
-        <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-black text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
-          years
-        </div>
-      </div>
-      <div className="flex items-start gap-[9px] relative self-stretch w-full flex-[0_0_auto]">
-        <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-purple text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
-          - -
-        </div>
-        <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-black text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
-          months
-        </div>
-      </div>
-      <div className="flex items-start gap-[8px] relative self-stretch w-full flex-[0_0_auto]">
-        <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-purple text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
-          - -
-        </div>
-        <div className="relative w-fit mt-[-1.00px] font-heading-l font-[number:var(--heading-l-font-weight)] [font-style:var(--heading-l-font-style)] text-black text-[length:var(--heading-l-font-size)] tracking-[var(--heading-l-letter-spacing)] leading-[var(--heading-l-line-height)] whitespace-nowrap">
-          days
-        </div>
-      </div>
+      <Age label="years" />
+      <Age label="months" />
+      <Age label="days" />
     </div>
   );
 }
