163e29367889b21ea9ed44aea834fc41844526ae
defy <8841170+defydef@users.noreply.github.com>
2023-11-03 08:48:10 +0700

    Complete AgeContext

--- src/contexts/AgeContext.jsx
+++ src/contexts/AgeContext.jsx
@@ -1,7 +1,14 @@
-import { createContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 // 1) Create a new Context component
 const AgeContext = createContext();
+
+const initialState = {
+  date: 0,
+  month: 0,
+  year: 0,
+  error: [],
+};
 
 function reducer(state, action) {
   switch (action.type) {
@@ -17,3 +24,27 @@ function reducer(state, action) {
       throw new Error("Undefined action");
   }
 }
+
+function AgeProvider({ children }) {
+  const [{ date, month, year, error }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
+
+  return (
+    <AgeContext.Provider value={{ date, month, year, error, dispatch }}>
+      {children}
+    </AgeContext.Provider>
+  );
+}
+
+function useAge() {
+  const context = useContext(AgeContext);
+  if (context === undefined)
+    throw new Error(
+      "useAge is undefined because it is defined inside the children component of App"
+    );
+  return context;
+}
+
+export { AgeProvider, useAge };
