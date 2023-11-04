0b504e6b727f276a1910d520920250c9d7ef40cd
defy <8841170+defydef@users.noreply.github.com>
2023-11-04 16:36:39 +0700

    Add error message for invalid date

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,49 @@ import { useAge } from "./contexts/AgeContext";
 
 function App() {
   const { dispatch, day, month, year } = useAge();
+  function isLeapYear(year) {
+    // Leap years are divisible by 4
+    if (year % 4 !== 0) {
+      return false;
+    }
+    // years divisible by 100 are not leap years unless they are also divisible by 400
+    if (year % 100 !== 0 || (year % 100 === 0 && year % 400 === 0)) {
+      return true;
+    }
+    return false;
+  }
+
+  function getInvalidDateError(day, month, year) {
+    const mon30 = [4, 6, 9, 11];
+    if (
+      (mon30.includes(month) && day === 31) ||
+      (month === 2 && isLeapYear(year) && (day === 30 || day === 31)) ||
+      (month === 2 && !isLeapYear(year) && day > 28 && day < 32)
+    )
+      return "Invalid date";
+    return null;
+  }
 
   function submitDOB(e) {
     e.preventDefault();
     const today = new Date();
-
+    const submittedDate = new Date(year, month - 1, day);
     const dayError =
       day < 1 || day > 31 ? "date is not within valid range" : null;
     const monthError =
       month < 1 || month > 12 ? "month is not within valid range" : null;
     const futureDateError =
       submittedDate > today ? "date should not be in the future" : null;
-    const submittedDate = new Date(year, month - 1, day);
-    console.log(submittedDate < today);
+    const invalidDateError = getInvalidDateError(day, month, year);
 
-    const isValidInput = !dayError && !monthError && !futureDateError;
+    const isValidInput =
+      !dayError && !monthError && !futureDateError && !invalidDateError;
 
     dispatch({
       type: "submit",
-      payload: isValidInput ? null : { dayError, monthError, futureDateError },
+      payload: isValidInput
+        ? null
+        : { dayError, monthError, futureDateError, invalidDateError },
     });
   }
 
