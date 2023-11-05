788925469810b6246725f0171a0e54f65af1168d
defy <8841170+defydef@users.noreply.github.com>
2023-11-05 15:54:23 +0700

    Add aria-label on button

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -3,6 +3,7 @@ function Button({ children, onClick }) {
     <button
       className="bg-[var(--purple)] hover:bg-[var(--black)] rounded-full w-16 h-16 sm:w-24 sm:h-24 flex justify-center items-center z-10 duration-200"
       onClick={onClick}
+      aria-label="submit"
     >
       {children}
     </button>
