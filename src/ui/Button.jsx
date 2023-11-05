3cf550d73a45dd5c8ee421de49234e3e89e277ce
defy <8841170+defydef@users.noreply.github.com>
2023-11-05 11:18:48 +0700

    change button background color on hover

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,5 +1,12 @@
 function Button({ children, onClick }) {
-  return <button onClick={onClick}>{children}</button>;
+  return (
+    <button
+      className="bg-[var(--purple)] hover:bg-[var(--black)] rounded-full w-16 h-16 sm:w-24 sm:h-24 flex justify-center items-center z-10 duration-200"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
 }
 
 export default Button;
