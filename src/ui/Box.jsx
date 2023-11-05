50814dd46b1c218c4d29ff393ce5b3a05c0b66e9
defy <8841170+defydef@users.noreply.github.com>
2023-11-05 16:00:50 +0700

    Reduce gap

--- src/ui/Box.jsx
+++ src/ui/Box.jsx
@@ -1,6 +1,6 @@
 function Box({ children }) {
   return (
-    <div className="flex flex-col sm:w-[52.5rem] w-[21.4375rem] h-[80vh] sm:h-fit items-center sm:p-[56px] bg-white rounded-[1.5rem_1.5rem_6.25rem_1.5rem] sm:rounded-[1.5rem_1.5rem_12.5rem_1.5rem] p-[3rem_1.5rem] gap-6">
+    <div className="flex flex-col sm:w-[52.5rem] w-[21.4375rem] h-[80vh] sm:h-fit items-center sm:p-[56px] bg-white rounded-[1.5rem_1.5rem_6.25rem_1.5rem] sm:rounded-[1.5rem_1.5rem_12.5rem_1.5rem] p-[3rem_1.5rem] gap-5">
       {children}
     </div>
   );
